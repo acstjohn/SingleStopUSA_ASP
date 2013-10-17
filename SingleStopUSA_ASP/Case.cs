8f5463316ccc1153b7d0a356ba3f23e0721d28c3
acstjohn <acstjohn@gmail.com>
2013-10-16 22:38:29 -0400

    updated with sdk classes and takes input from form

---
+++ SingleStopUSA_ASP/Case.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingleStopUSA_ASP
+{
+    public class Query
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ SingleStopUSA_ASP/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingleStopUSA_ASP
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
